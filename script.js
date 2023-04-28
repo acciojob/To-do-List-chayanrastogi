cbc8be3f8b1d65edfed23dcb46f919436baa6405
Chayan Rastogi <80814459+chayanrastogi@users.noreply.github.com>
2023-04-28 22:22:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-//your code here
+function handleAdd(){
+	var input = document.getElementById("newTodoInput").value;
+	var defVal = document.getElementById("newTodoInput").defaultValue;
+	if(input !== defVal){
+		const newLi = document.createElement('li');
+		newLi.textContent = input;
+		const ol = document.querySelector("ol");
+		ol.appendChild(newLi);
+		document.getElementById("newTodoInput").value = ""
+	}
+}
 
